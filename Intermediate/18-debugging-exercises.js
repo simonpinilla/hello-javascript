6671fc94a533346def91caf707ee3d39a9f6717f
Simón <simon.pinilla10@gmail.com>
2025-04-24 13:50:35 -0400

    limpieza repositorio

--- Intermediate/18-debugging-exercises.js
+++
@@ -1,8 +0,0 @@
-/*
-Clase 7 - Debug, Regex y Testing (12/03/2025)
-Vídeo: https://www.twitch.tv/videos/2420494093?t=00h21m55s
-*/
-
-// 1. Crea un código con un error lógico y usa VS Code para encontrarlo
-
-// 2. Experimenta con breakpoints y observa cómo cambia el flujo de ejecución
